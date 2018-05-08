271ce5b9da43c838e41ac0055c441fc12e49369e
Ken Sharman <kenneth.sharman@ucalgary.ca>
2018-05-08 00:12:05 -0600

    Finished Animation

--- radial_keeling_curve_animation.py
+++ radial_keeling_curve_animation.py
@@ -25,39 +25,38 @@ def yearly_average(array):
 
 yearly_averages = yearly_average(data['trend'])
 years = np.arange(1958, 2018, 1)
+select_data = [i for i in zip(yearly_averages, years)] 
 
 from matplotlib import pyplot as plt
 from matplotlib import animation
 from IPython.display import HTML
 
+
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure(figsize=(15,10))
+fig.suptitle('$CO_2$ Concentration at Mauna Loa Observatory, Hawaii', fontsize=20)
 ax = plt.axes(polar=True)
-ax.set_ylim(310,400)
+ax.set_ylim(310,500)
 line, = ax.plot([], [], lw=2)
-title = ax.text (0.02, 1.0, 'TITLE', fontsize=14, transform=ax.transAxes)
+title = ax.text (0.02, 1.0, '', fontsize=16, transform=ax.transAxes)
+title1 = ax.text(0.02, 0.0, '', fontsize=16, transform=ax.transAxes)
 
 # initialization function: plot the background of each frame
 def init():
     line.set_data([], [])
-    title.set_text ('NOPE')
-    return line, title
+    return line
 
-# animation function.  This is called sequentially
+# animation function.
 def animate(i):
     theta = np.arange(0,2.1*np.pi, 0.1)
     r = np.array([0])*len(theta)+i[0]
     line.set_data(theta, r)
-    title.set_text (i[1].__str__())
+    title.set_text ('Year: ' + i[1].__str__())
+    title1.set_text ('CO2: ' + np.round(i[0], 2).__str__() + ' ppm')
     return line
 
-# call the animator.  blit=True means only re-draw the parts that have changed.
-'''
-for msmt in yearly_averages:
-    anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=60, interval=20, blit=False, fargs=(msmt,))
-    HTML(anim.to_jshtml())
-'''
+# call the animator.
 anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=select_data, interval=200, blit=False, repeat=False)
+                               frames=select_data, interval=200, blit=False, repeat=True)
 HTML(anim.to_jshtml())
+#anim.save('keeling_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
