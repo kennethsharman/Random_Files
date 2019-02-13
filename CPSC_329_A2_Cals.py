958141c3de3c5530a35372047f25acdc3b21980e
Kenneth Sharman <kenneth.sharman@ucalgary.ca>
2019-02-13 01:36:31 -0700

    A2 - Started Q2

--- CPSC_329_A2_Cals.py
+++ CPSC_329_A2_Cals.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 from math import factorial
 from math import ceil
 
+# Question 1
+
 # 100 possible passfaces, user assigned random 5
 # C(100,5)
 num_pass_set = factorial(100) / ( factorial(100-5)*factorial(5) )
@@ -37,3 +39,31 @@ probability_4_or_5of5 = probability_4of5 + probability_5of5
 percent_4_or_5of5 = probability_4_or_5of5 * 100
 print('Probability of Correctly Guessing when 1 mistake is allowed = %.3e' % probability_4_or_5of5)
 print('%.3e' % percent_4_or_5of5, '%')
+
+# Question 2
+# method 1
+
+# 100 possible passfaces, user selects 20
+# C(100,20)
+num_pass_set2 = factorial(100) / ( factorial(100-20)*factorial(20) )
+print('Number of possible passface sets = %.3e' % num_pass_set2)
+
+prob_guessing = 1.0/num_pass_set2
+print('Probability of correctly guessing the set of 20 = %.3e' % prob_guessing )
+percent_prob_guessing = 100.0 * prob_guessing
+print('%.3e' % percent_prob_guessing, '%')
+
+# Enropy = ceil(log_2(x))
+entropy2 = ceil( np.log(num_pass_set2)/np.log(2) )
+print('Entropy of 20 passface set', entropy2)
+
+# method 2
+
+# probability of correctly responding to 20 challenges
+# each challenge has 2 pics: 1 right and 1 wrong
+
+prob2 = (1.0/2)**20
+print('Probability of correctly responding to all 20 challenges %.3e' % prob2)
+
+percent_prob2 = prob2 * 100.0
+print('%.3e' % percent_prob2, '%')
